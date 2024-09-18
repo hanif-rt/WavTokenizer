a843876dce6de54fc6d2a5edfe6cb442dc4a764b
hanif-rt <167826791+hanif-rt@users.noreply.github.com>
2024-09-17 17:13:53 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wavtokenizer",
+    version="0.1.0",
+    url="https://github.com/jishengpeng/WavTokenizer",
+    author="hihih",
+    author_email="hanif@reflex.tech",
+    packages=find_packages(),
+)
