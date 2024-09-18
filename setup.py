10deb97221654f3e4d0dedec99e334aab3a0e53b
hanif-rt <167826791+hanif-rt@users.noreply.github.com>
2024-09-17 17:15:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="wavtokenizer",
+    name="WavTokenizer",
     version="0.1.0",
-    url="https://github.com/jishengpeng/WavTokenizer",
+    url="https://github.com/hanif-rt/WavTokenizer",
     author="hihih",
     author_email="hanif@reflex.tech",
     packages=find_packages(),
